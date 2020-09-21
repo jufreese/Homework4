e5d84ae0b24b3e9dc463d30d7e2407c055088cb7
jufreese <70898896+jufreese@users.noreply.github.com>
2020-09-20 21:53:24 -0400

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -4,29 +4,20 @@ using namespace std;
 
 int main()
 {
-
 	int i, n;
 	int * p;
 
 	cout << "Please enter number of cities: ";
 	cin >> i;
+
 	p = new (nothrow) int[i];
-	if (p == nullptr)
-		cout << "Error: memory could not be allocated";
-	else
-	{
 		for (n = 0; n < i; n++)
 		{
 			cout << "Enter the population of city " << n + 1 << ": ";
 			cin >> p[n];
 		}
-
-
 		for (n = 0; n < i; n++)
 			cout << "City " << n + 1 << ": " << p[n] << endl;
-
-		delete[] p;
-
 	}
 	return 0;
 }
